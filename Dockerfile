e4f7324f7fabedf28d98c56da3eb5f303948a667
MemphiZ <MemphiZ@X-ToolZ.com>
2018-09-22 22:40:01 +0200

    RC6

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,15 @@
-FROM postgres:9
+FROM postgres:10
 
-RUN pg_createcluster 9.6 main
-RUN apt update && \
-apt install gcc g++ wget cmake ccache zlib1g-dev qtdeclarative5-dev qtscript5-dev libqt5sql5-psql -y
-RUN cd /root/ && \
-wget https://quassel-irc.org/pub/quassel-0.13-rc1.tar.bz2 && \
-tar -xvf quassel*tar.bz2 && \
-rm quassel-*.tar.bz2 && \
-cd quassel-* && \
-mkdir build && \
-cd build && \
-cmake .. -DWANT_QTCLIENT=OFF -DWANT_MONO=OFF && \
-make -j$(nproc) && \
-make install && \
-rm /root/quassel-* -R
-RUN mkdir -p /var/lib/postgresql/.config/quassel-irc.org/
-RUN openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /var/lib/postgresql/.config/quassel-irc.org/quasselCert.pem -out /var/lib/postgresql/.config/quassel-irc.org/quasselCert.pem -subj "/C=DE/ST=X/L=X/O=X/OU=X/CN=X"
-RUN chown postgres:postgres /var/lib/postgresql/.config/quassel-irc.org/ -R
-RUN apt remove --purge gcc g++ cmake ccache zlib1g-dev qtdeclarative5-dev qtscript5-dev -y
-RUN apt install libqt5script5 libqt5network5 -y
-RUN apt autoremove -y
-RUN apt clean
+RUN pg_createcluster 10 main
+RUN apt update && apt install nano wget quassel-core libqt5sql5-psql -y
+RUN rm /var/lib/quassel/quasselCert.pem
+RUN openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /var/lib/quassel/quasselCert.pem -out /var/lib/quassel/quasselCert.pem -subj "/C=DE/ST=X/L=X/O=X/OU=X/CN=X"
+RUN echo 'false' > /var/lib/quassel/POSTGRES_READY && chmod ug+rw /var/lib/quassel/POSTGRES_READY
+RUN chown quasselcore:postgres /var/lib/quassel/ -R
+RUN adduser postgres quassel
+RUN adduser quasselcore postgres
 
-COPY docker-entrypoint.sh /usr/local/bin/
+COPY docker-entrypoint-postgres10.sh /usr/local/bin/docker-entrypoint.sh
 RUN ln -sf /usr/local/bin/docker-entrypoint.sh / # backwards compat
 COPY QuasselCoreHelper.sh /usr/local/bin/
 ENTRYPOINT ["docker-entrypoint.sh"]
--- QuasselCoreHelper.sh
+++ QuasselCoreHelper.sh
@@ -1,31 +1,31 @@
-echo "QuasselCoreHelper: initializing.."
-while [ -z ${POSTGRES_READY+x} ]
-do 
-    echo "QuasselCoreHelper: ready/waiting.."
-    sleep 5s    
+#!/usr/bin/env bash
+while [ 'false' == $(</var/lib/quassel/POSTGRES_READY) ]
+do
+	echo "QuasselCoreHelper: ready/waiting.."
+	sleep 5s
 done 
-
+echo 'false' > /var/lib/quassel/POSTGRES_READY
 while :
 do
-    echo "QuasselCoreHelper: Connecting to database.."
-    db_connect=$(psql -h localhost -U $POSTGRES_USER -lqt >/dev/null 2>&1)
-    exitcode=$?
-    if [ $exitcode -ne 0 ]; then
-        echo "QuasselCoreHelper: Database connection failed!, retrying in 10 seconds.."
-        sleep 10s
-    else
-        db_exists=$(psql -h localhost -U $POSTGRES_USER -lqt quassel >/dev/null 2>&1)
-        exitcode=$?
-        if [ $exitcode -ne 0 ]; then
-            echo echo "QuasselCoreHelper: Database 'quassel' does not exist!, creating.."
-            psql -h localhost -U admin -c "CREATE DATABASE quassel OWNER $POSTGRES_USER" >/dev/null 2>&1
-            psql -h localhost -U admin -c "GRANT ALL PRIVILEGES ON DATABASE quassel TO $POSTGRES_USER" >/dev/null 2>&1
-        else
-            echo "QuasselCoreHelper: Database is up!, starting QuasselCore.."
-            /usr/local/bin/quasselcore --configdir=/var/lib/postgresql/.config/quassel-irc.org/
-            kill -INT $(head -1 /var/lib/postgresql/data/postmaster.pid)
-            break
-        fi
-    fi
+	echo "QuasselCoreHelper: Connecting to database.."
+	db_connect=$(psql -h localhost -U $POSTGRES_USER -lqt >/dev/null 2>&1)
+	exitcode=$?
+	if [ $exitcode -ne 0 ]; then
+		echo "QuasselCoreHelper: Database connection failed!, retrying in 10 seconds.."
+		sleep 10s
+	else
+		db_exists=$(psql -h localhost -U $POSTGRES_USER -lqt quassel >/dev/null 2>&1)
+		exitcode=$?
+		if [ $exitcode -ne 0 ]; then
+			echo echo "QuasselCoreHelper: Database 'quassel' does not exist!, creating.."
+			psql -h localhost -U admin -c "CREATE DATABASE quassel OWNER $POSTGRES_USER" >/dev/null 2>&1
+			psql -h localhost -U admin -c "GRANT ALL PRIVILEGES ON DATABASE quassel TO $POSTGRES_USER" >/dev/null 2>&1
+		else
+			echo "QuasselCoreHelper: Database is up!, starting QuasselCore.."
+			/usr/bin/quasselcore --configdir=/var/lib/quassel/
+			kill -INT $(head -1 /var/lib/postgresql/data/postmaster.pid)
+			break
+		fi
+	fi
 done
 
